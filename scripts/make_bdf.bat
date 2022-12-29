f2ae5507aa807ce60dc4b1629b2644a7228a0f1b
Boguslaw Czwartkowski <czwartek@parasoft.com>
2022-12-29 23:15:27 +0100

    Switch to "Official release with BLE Remocon - 170318", created C/C++test project, BDF

--- scripts/make_bdf.bat
+++ scripts/make_bdf.bat
@@ -1,9 +1,11 @@
 @echo on
 
 set PROJECT_DIR=%~dp0\..\STM32 FW Project\Official latest release 221117
+set PROJECT_DIR=%~dp0\..\STM32 FW Project\Official release with BLE Remocon - 170318
 call %~dp0\setenv_iar_ewarm.bat
 
 pushd "%PROJECT_DIR%\EWARM"
+del /q /s .\cpptest\cpptestscan.bdf
 iarbuild ToyDrone.ewp -clean cpptest -log all
 cpptesttrace --cpptesttraceResponseFileOption=-f iarbuild ToyDrone.ewp -make cpptest -log all
 dir cpptest
