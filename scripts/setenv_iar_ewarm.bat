040f05c7df6a32fe5ce901018ef580cb83d93a5d
Boguslaw Czwartkowski <Boguslaw.Czwartkowski@parasoft.com>
2022-12-29 11:25:48 +0100

    Added setenv script for IAR EWARM and notes document

---
+++ scripts/setenv_iar_ewarm.bat
@@ -0,0 +1,6 @@
+set PATH=c:\Program Files\IAR Systems\Embedded Workbench 9.1\common\bin;c:\Program Files\IAR Systems\Embedded Workbench 9.1\arm\bin;c:\Parasoft\apps\cpptest-pro-2022.2.0;c:\Parasoft\apps\cpptest-pro-2022.2.0\bin;%PATH%
+
+iccarm --version
+ilinkarm --version
+rem iarbuild --version
+cpptestcli -version
