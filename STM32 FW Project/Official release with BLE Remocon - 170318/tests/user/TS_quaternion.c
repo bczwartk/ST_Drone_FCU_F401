0e92c178c27a7cd95866dce6fc50eb6857aa1a35
Boguslaw Czwartkowski <czwartek@parasoft.com>
2023-01-04 11:41:22 +0100

    quaternion.c unit tests - WIP

--- STM32 FW Project/Official release with BLE Remocon - 170318/tests/user/TS_quaternion.c
+++ STM32 FW Project/Official release with BLE Remocon - 170318/tests/user/TS_quaternion.c
@@ -13,10 +13,14 @@ CPPTEST_TEST_SUITE(TS_quaternion);
         CPPTEST_TEST_SUITE_SETUP(TS_quaternion_testSuiteSetUp);
         CPPTEST_TEST_SUITE_TEARDOWN(TS_quaternion_testSuiteTearDown);
 CPPTEST_TEST(TS_quaternion_test_QuaternionConj);
+CPPTEST_TEST_DS(TS_quaternion_test_QuaternionNorm_ds, CPPTEST_DS("TS_Quaternion_Norm"));
+CPPTEST_TEST_DS(TS_quaternion_test_QuaternionMult_ds, CPPTEST_DS("TS_Quaternion_Mult"));
 CPPTEST_TEST_SUITE_END();
         
 
 void TS_quaternion_test_QuaternionConj(void);
+void TS_quaternion_test_QuaternionNorm_ds(void);
+void TS_quaternion_test_QuaternionMult_ds(void);
 CPPTEST_TEST_SUITE_REGISTRATION(TS_quaternion);
 
 void TS_quaternion_testSuiteSetUp(void);
@@ -78,3 +82,65 @@ void TS_quaternion_test_QuaternionConj()
     }
 }
 /* CPPTEST_TEST_CASE_END test_QuaternionConj */
+
+/* CPPTEST_TEST_CASE_BEGIN test_QuaternionNorm_ds */
+/* CPPTEST_TEST_CASE_CONTEXT void QuaternionNorm(QuaternionTypeDef *) */
+void TS_quaternion_test_QuaternionNorm_ds()
+{
+    /* Pre-condition initialization */
+    /* Initializing argument 1 (q) */ 
+    QuaternionTypeDef _q_0 ;
+     _q_0.q0  = CPPTEST_DS_GET_FLOAT("q0_in");
+     _q_0.q1  = CPPTEST_DS_GET_FLOAT("q1_in");
+     _q_0.q2  = CPPTEST_DS_GET_FLOAT("q2_in");
+     _q_0.q3  = CPPTEST_DS_GET_FLOAT("q3_in");
+    QuaternionTypeDef * _q  = & _q_0;
+    {
+        /* Tested function call */
+        QuaternionNorm(_q);
+        /* Post-condition check */
+        CPPTEST_ASSERT_FLOAT_EQUAL(CPPTEST_DS_GET_FLOAT("q0_out"), _q_0.q0, 0.001);
+        CPPTEST_ASSERT_FLOAT_EQUAL(CPPTEST_DS_GET_FLOAT("q1_out"), _q_0.q1, 0.001);
+        CPPTEST_ASSERT_FLOAT_EQUAL(CPPTEST_DS_GET_FLOAT("q2_out"), _q_0.q2, 0.001);
+        CPPTEST_ASSERT_FLOAT_EQUAL(CPPTEST_DS_GET_FLOAT("q3_out"), _q_0.q3, 0.001);
+    }
+}
+/* CPPTEST_TEST_CASE_END test_QuaternionNorm_ds */
+
+/* CPPTEST_TEST_CASE_BEGIN test_QuaternionMult_ds */
+/* CPPTEST_TEST_CASE_CONTEXT void QuaternionMult(QuaternionTypeDef *, QuaternionTypeDef *, QuaternionTypeDef *) */
+void TS_quaternion_test_QuaternionMult_ds()
+{
+    /* Pre-condition initialization */
+    /* Initializing argument 1 (qa) */ 
+    QuaternionTypeDef _qa_2 ;
+     _qa_2.q0  = CPPTEST_DS_GET_FLOAT("qa_q0");
+     _qa_2.q1  = CPPTEST_DS_GET_FLOAT("qa_q1");
+     _qa_2.q2  = CPPTEST_DS_GET_FLOAT("qa_q2");
+     _qa_2.q3  = CPPTEST_DS_GET_FLOAT("qa_q3");
+    QuaternionTypeDef * _qa  = & _qa_2;
+    /* Initializing argument 2 (qb) */ 
+    QuaternionTypeDef _qb_4 ;
+     _qb_4.q0  = CPPTEST_DS_GET_FLOAT("qb_q0");
+     _qb_4.q1  = CPPTEST_DS_GET_FLOAT("qb_q1");
+     _qb_4.q2  = CPPTEST_DS_GET_FLOAT("qb_q2");
+     _qb_4.q3  = CPPTEST_DS_GET_FLOAT("qb_q3");
+    QuaternionTypeDef * _qb  = & _qb_4;
+    /* Initializing argument 3 (qo) */ 
+    QuaternionTypeDef _qo_6 ;
+     _qo_6.q0  = 0.0f;
+     _qo_6.q1  = 0.0f;
+     _qo_6.q2  = 0.0f;
+     _qo_6.q3  = 0.0f;
+    QuaternionTypeDef * _qo  = & _qo_6;
+    {
+        /* Tested function call */
+        QuaternionMult(_qa, _qb, _qo);
+        /* Post-condition check */
+        CPPTEST_ASSERT_FLOAT_EQUAL(CPPTEST_DS_GET_FLOAT("qo_q0"), _qo->q0, 0.001);
+        CPPTEST_ASSERT_FLOAT_EQUAL(CPPTEST_DS_GET_FLOAT("qo_q1"), _qo->q1, 0.001);
+        CPPTEST_ASSERT_FLOAT_EQUAL(CPPTEST_DS_GET_FLOAT("qo_q2"), _qo->q2, 0.001);
+        CPPTEST_ASSERT_FLOAT_EQUAL(CPPTEST_DS_GET_FLOAT("qo_q3"), _qo->q3, 0.001);
+    }
+}
+/* CPPTEST_TEST_CASE_END test_QuaternionMult_ds */
