08df2e96b5c9f7a678eb3a5122ebb326c7bdf571
Boguslaw Czwartkowski <czwartek@poczta.fm>
2024-10-26 14:49:29 +0200

    Switch to 2023.2.1, update IAR location

--- scripts/setenv_iar_ewarm.bat
+++ scripts/setenv_iar_ewarm.bat
@@ -3,12 +3,8 @@
 rem setlocal ENABLEDELAYEDEXPANSION
 
 rem the defaults
-set CPPTEST_HOME=c:\Parasoft\apps\cpptest-pro-2023.1.1
+set CPPTEST_HOME=c:\Parasoft\apps\cpptest-pro-2023.2.1
 set IAR_HOME=c:\Program Files\IAR Systems\Embedded Workbench 9.1
-
-IF %COMPUTERNAME%==HOUND2 (
-    set IAR_HOME=E:\apps\EWARM_9.1
-)
 
 set PATH=%IAR_HOME%\common\bin;%IAR_HOME%\arm\bin;%CPPTEST_HOME%;%CPPTEST_HOME%\bin;%PATH%
 
